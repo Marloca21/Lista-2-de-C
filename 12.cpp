1f90f882ac25d436d1d0b54b4c9eade1380d3762
Márlon de Souza <marlondesouzajardim@gmail.com>
2021-09-07 16:22:22 -0300

    Update 12.cpp

--- 12.cpp
+++ 12.cpp
@@ -1,24 +1,24 @@
-//12.Criar um algoritmo que leia os elementos de uma matriz inteira 4 x 4 e 
-//escreva os elementos da diagonal principal. 
+//12.Criar um algoritmo que leia os elementos de uma matriz inteira 4 x 4 e
+//escreva os elementos da diagonal principal.
 
 #include <stdio.h>
 #include <stdlib.h>
 #include <locale.h>
 
-int main(){
+int main() {
 
-   setlocale(LC_ALL, "Portuguese");
+	setlocale(LC_ALL, "Portuguese");
 	int lin,col, tab;
 	int mat[4][4];
-	
+
 	for (lin = 0; lin < 4; lin++) {
 		for (col = 0; col < 4; col++) {
 			printf ("Digite ELEMENTO da linha %d, coluna %d da matriz: ",lin+1,col+1);
 			scanf ("%d", &mat[lin][col]);
 		}
 	}
-	
-	printf("Matriz\n");
+
+	printf("\nMatriz\n");
 	for (lin = 0; lin < 4; lin++) {
 		for (col = 0; col < 4; col++)
 			printf("%d\t",mat[lin][col]);
@@ -31,5 +31,4 @@ int main(){
 		for (tab=1; tab<=lin+1; tab++)
 			printf("\t");
 	}
-	
 }
